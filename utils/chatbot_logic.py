24a6924e428c6df086f5f41b0a6e49998976a7a6
Munavar <foloex@gmail.com>
2025-08-17 12:47:21 +0530

    Initial commit

---
+++ utils/chatbot_logic.py
@@ -0,0 +1,11 @@
+class WhatsAppHandler:
+    def __init__(self):
+        # This runs when you create a new WhatsAppHandler
+        # It sets up all the settings
+        pass
+    
+    def send_message(self, to_number, message_text):
+        # This function sends a message to someone
+        # to_number = who to send to
+        # message_text = what to say
+        pass
